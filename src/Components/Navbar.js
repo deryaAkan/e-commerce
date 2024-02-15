5b9f608e46e930355b173baf906c94ef37b6f8c2
Derya Akan <d.akan369@gmail.com>
2024-02-15 20:58:16 +0300

    navbar login part done

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -14,9 +14,9 @@ export default function Dropdown() {
 
   return (
     <>
-      <div className="relative flex gap-2" onBlur={closeMenu}>
+      <div className="relative flex  gap-2" onBlur={closeMenu}>
         <Link to="/">Home</Link>
-        <div>
+        <div className="relative flex text-left">
           <button
             onClick={toggleMenu}
             className="inline-flex w-full justify-center gap-x-1.5 text-sm text-gray-900"
@@ -41,7 +41,7 @@ export default function Dropdown() {
         </div>
         {menuOpen && (
           <div
-            className="absolute left-0 z-10 mt-4 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+            className="absolute left-0 z-10 mt-7 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
             role="menu"
             aria-orientation="vertical"
             aria-labelledby="menu-button"
--- src/Layouts/Header.js
+++ src/Layouts/Header.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
@@ -10,13 +9,17 @@ import {
   faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
 import Dropdown from "../Components/Navbar";
-
+import loginSvg from '../assets/header/loginvector.svg';
+import shopCardSvg from '../assets/header/shopCardSvg.svg';
+import searchSvg from '../assets/header/searchBar.svg';
+import favsSvg from '../assets/header/favs.svg';
 
 export default function Header() {
   return (
     <>
       <header className="CommonThings w-full flex flex-col">
-        <div className="flex w-full bg-[#252B42] justify-around flex-wrap sm:hidden">
+        <div className="flex w-full bg-[#252B42] justify-center pr-6 pl-6">
+        <div className="flex w-full justify-between flex-wrap sm:hidden pt-3 pb-3">
           <div className="flex gap-2">
             <div className="flex items-center text-sm font-bold gap-2">
               <FontAwesomeIcon icon={faPhone} style={{ color: "#ffffff" }} />
@@ -27,9 +30,9 @@ export default function Header() {
               <h6>michelle.rivera@example.com</h6>
             </div>
           </div>
-          <div>
+          <div className="flex">
             {" "}
-            <h6>Follow Us and get a chance to win 80% off</h6>
+            <h6 className="font-bold text-sm">Follow Us and get a chance to win 80% off</h6>
           </div>
           <div className="flex items-center text-sm font-bold gap-2">
             <h6>Follow Us:</h6>
@@ -44,23 +47,30 @@ export default function Header() {
             </div>
           </div>
         </div>
-        <div className="flex justify-around w-4/5">
-          <div className="text-[#252B42] flex items-center flex-grow">
+        </div>
+        <div className="flex w-full justify-around flex-wrap pr-6 pl-6">
+          <div className="text-[#252B42] flex items-center flex-grow gap-24">
             <h3 className="font-2xl font-bold">Bandage</h3>
-            <nav className="flex text-sm">
+            <nav className="flex text-sm font-bold text-gray-400">
               <Dropdown />
             </nav>
           </div>
-          <div className="text-[#252B42] flex items-center gap-2 flex-grow justify-end">
-            <p>Login</p>
-            <p>Register</p>
-            <p>o</p>
-            <p>o</p>
-            <p>o</p>
+          <div className="text-[#23A6F0] text-sm flex items-center gap-2 flex-grow justify-end">
+            <img src={loginSvg} />
+            <a className="font-bold" href="#" >Login /</a>
+            <a className="font-bold" href="#">Register</a>
+            <a href="#">
+              <img src={searchSvg} />
+            </a>
+            <a href="#">
+            <img src={shopCardSvg} />
+            </a>
+            <p>1</p>
+            <a href="#">
+            <img src={favsSvg} />
+            </a>
+            <p>1</p>
           </div>
-        </div>
-        <div>
-          
         </div>
       </header>
     </>
