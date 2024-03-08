c6ebdd63ef532162d88c8a6621a39b0b2da6c2f3
Derya Akan <d.akan369@gmail.com>
2024-03-08 12:08:27 +0300

    LoginForm redirection problem fixed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import AboutPage from "./Pages/AboutPage";
 import ContactPage from "./Pages/ContactPage";
 import ProductPage from "./Pages/ProductPage";
 import SignUpForm from "./Components/SignUpForm";
-import LoginForm1 from "./Components/LoginForm1";
+import LoginForm from "./Components/LoginForm";
 
 function App() {
   return (
@@ -26,7 +26,7 @@ function App() {
         <Route path="/contact" component={ContactPage} />
         <Route path="/productpage" component={ProductPage} />
         <Route path="/signup" component={SignUpForm} />
-        <Route path="/login" component={LoginForm1} />
+        <Route path="/login" component={LoginForm} />
       </Switch>
       <Footer />
       <ToastContainer />
