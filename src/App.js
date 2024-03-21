7cd561b2d35793c241a5e36d6ed39340a7669cab
Derya Akan <d.akan369@gmail.com>
2024-03-21 22:50:28 +0300

    T8: local storage e-mail & password (client)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./Layouts/Footer";
 import Header from "./Layouts/Header";
 import Home from "./Pages/Home";
 import Shop from "./Pages/Shop";
-import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import {
+  Route,
+  Switch,
+  useHistory,
+} from "react-router-dom/cjs/react-router-dom.min";
 import TeamPage from "./Pages/TeamPage";
 import AboutPage from "./Pages/AboutPage";
 import ContactPage from "./Pages/ContactPage";
 import ProductPage from "./Pages/ProductPage";
 import SignUpForm from "./Components/SignUpForm";
 import LoginForm from "./Components/LoginForm";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 
 function App() {
+  const baseURL = "https://workintech-fe-ecommerce.onrender.com";
+  const instance = axios.create({ baseURL });
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const userReducerData = useSelector((store) => store.userRed);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const userName = localStorage.getItem("userName");
+    if (userName) {
+      toast.success(`${userName} Welcome!`);
+    }
+    if (token) {
+      instance
+        .get("/verify", {
+          headers: {
+            Authorization: token,
+          },
+        })
+        .then((res) => {
+          console.log("Auto login", res.data);
+          localStorage.setItem("token", res.data.token);
+          localStorage.setItem("userName", res.data.name);
+        })
+        .catch((err) => {
+          console.error("login hata", err);
+          localStorage.removeItem("token");
+        });
+    }
+  }, []);
+
   return (
     <div className="App">
       <Header />
