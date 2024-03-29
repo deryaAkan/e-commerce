c2c73cdca7fbcee9ab270867ff1aaf8312b1df34
Derya Akan <d.akan369@gmail.com>
2024-03-29 11:38:30 +0300

    categories listed on the shop page

--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -1,11 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "../Components/ProductCard";
 import Categories from "../Components/Categories";
 import Companies from "../Layouts/Companies";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import imageee from "../assets/homepage/product-cover-5productPage1.png";
+import { useDispatch, useSelector } from "react-redux";
+import { getCategories } from "../store/actions/globalActions";
 
 const Shop = () => {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  const categoriesData = useSelector((store) => store.global.categories);
+
+  useEffect(() => {
+    setLoading(true);
+    const timeout = setTimeout(() => {
+      dispatch(getCategories());
+      setLoading(false);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, []);
+
   const [products, setProducts] = useState([
     {
       id: 1,
@@ -235,7 +250,40 @@ const Shop = () => {
     indexOfLastProduct
   );
 
+  const sortByRating = categoriesData.sort((a, b) => b.rating - a.rating);
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  if (loading) {
+    return (
+      <div className="relative">
+        <div className="absolute bg-white bg-opacity-60 z-10 h-full w-full top-24 flex items-center justify-center">
+          <div className="flex items-center">
+            <span className="text-3xl mr-4 ">Loading</span>
+            <svg
+              className="animate-spin h-8 w-8 text-gray-800"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col justify-center items-center w-full">
@@ -245,7 +293,37 @@ const Shop = () => {
         </div>
         <p className="text-sm text-gray-300 font-bold ">Home -- Shop</p>
       </div>
-      <Categories />
+      <span
+        className="flex flex-row justify-center gap-4 sm:flex-col sm:items-center"
+        id="box-cards"
+      >
+        {sortByRating.slice(0, 5).map((box, index) => (
+          <Link
+            key={index}
+            to={`/shop/${
+              box.gender === "e" ? "erkek" : "kadin"
+            }/${box.title.toLowerCase()}`}
+          >
+            <div
+              id="container"
+              className="relative shadow-lg sm:justify-center shadow-gray flex items-center sm:flex-col sm:w-fit  "
+            >
+              <img className="object-cover w-[250px] h-[250px]" src={box.img} />
+              <button
+                id="center"
+                className="absolute t-1/2 w-full text-center text-lg sm:text-xl text-white font-bold"
+              >
+                <p className="drop-shadow-4xl">{box.title}</p>
+                <p className="drop-shadow-4xl">Rating : {box.rating}</p>
+                <p className="drop-shadow-4xl">
+                  {box.gender === "e" ? "Erkek" : "Kadın"}
+                </p>
+              </button>
+            </div>
+          </Link>
+        ))}
+      </span>
+
       <div className="flex w-3/4 justify-between flex-wrap items-center py-6">
         <div className="text-[#252B42] w-full flex items-center justify-between flex-grow sm:flex-col sm:gap-5">
           <h3 className="text-sm text-gray-600 font-bold">
