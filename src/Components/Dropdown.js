50431174925d43bb4761502333bfda36b3971e83
Derya Akan <d.akan369@gmail.com>
2024-05-24 12:00:30 +0300

    T9 Dropdown menu links activated

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getCategories } from "../store/actions/globalActions";
@@ -7,51 +7,54 @@ export default function Dropdown() {
   const [menuOpen, setMenuOpen] = useState(false);
   const categoriesData = useSelector((store) => store.global.categories);
   const dispatch = useDispatch();
+  const dropdownRef = useRef(null);
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      dispatch(getCategories());
-    }, 300);
-    return () => clearTimeout(timeout);
-  }, []);
+    dispatch(getCategories());
+  }, [dispatch]);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
-  const closeMenu = () => {
-    setMenuOpen(false);
+  const closeMenu = (e) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+      setMenuOpen(false);
+    }
   };
 
+  useEffect(() => {
+    document.addEventListener("mousedown", closeMenu);
+    return () => {
+      document.removeEventListener("mousedown", closeMenu);
+    };
+  }, []);
+
   return (
-    <>
-      <div className="relative flex gap-3 sm:flex-col" onBlur={closeMenu}>
-        <Link to="/">Home</Link>
-        <div className="relative flex text-left">
-          <Link to="/shop">
-            <button
-              className="inline-flex w-full justify-center gap-x-1.5 text-sm text-gray-900"
-              id="menu-button"
-              aria-expanded={menuOpen}
-              aria-haspopup="true"
-            >
-              Shop
-              <svg
-                onClick={toggleMenu}
-                className="h-5 w-5 text-gray-900"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-          </Link>
-        </div>
+    <div className="relative flex gap-3 sm:flex-col" ref={dropdownRef}>
+      <Link to="/">Home</Link>
+      <div className="relative flex text-left">
+        <button
+          className="inline-flex w-full justify-center gap-x-1.5 text-sm text-gray-900"
+          id="menu-button"
+          aria-expanded={menuOpen}
+          aria-haspopup="true"
+          onClick={toggleMenu}
+        >
+          Shop
+          <svg
+            className="h-5 w-5 text-gray-900"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
         {menuOpen && (
           <div
             className="flex items-center justify-center absolute left-0 z-10 mt-7 w-56 origin-top-right divide-y divide-gray-100 bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
@@ -63,7 +66,7 @@ export default function Dropdown() {
             <div className="flex">
               <div className="WOMEN flex flex-col px-2">
                 <Link
-                  onClick={closeMenu}
+                  onClick={toggleMenu}
                   to="/shop/kadin"
                   className="text-gray-700 block px-4 py-2 text-sm"
                   role="menuitem"
@@ -77,7 +80,7 @@ export default function Dropdown() {
                   (box, index) =>
                     box.gender === "k" && (
                       <Link
-                        onClick={closeMenu}
+                        onClick={toggleMenu}
                         key={index}
                         to={`/shop/kadin/${box.title.toLowerCase()}`}
                         className="text-gray-400 block px-4 py-2 text-sm hover:text-[#00A1C1]"
@@ -85,7 +88,6 @@ export default function Dropdown() {
                         tabIndex="-1"
                         id="menu-item-0"
                       >
-                        {" "}
                         {box.title}
                       </Link>
                     )
@@ -93,7 +95,7 @@ export default function Dropdown() {
               </div>
               <div className="MEN flex flex-col">
                 <Link
-                  onClick={closeMenu}
+                  onClick={toggleMenu}
                   to="#"
                   className="text-gray-700 block px-4 py-2 text-sm"
                   role="menuitem"
@@ -107,7 +109,7 @@ export default function Dropdown() {
                   (box, index) =>
                     box.gender === "e" && (
                       <Link
-                        onClick={closeMenu}
+                        onClick={toggleMenu}
                         key={index}
                         to={`/shop/erkek/${box.title.toLowerCase()}`}
                         className="text-gray-400 block px-4 py-2 text-sm hover:text-[#00A1C1]"
@@ -115,7 +117,6 @@ export default function Dropdown() {
                         tabIndex="-1"
                         id="menu-item-0"
                       >
-                        {" "}
                         {box.title}
                       </Link>
                     )
@@ -124,11 +125,11 @@ export default function Dropdown() {
             </div>
           </div>
         )}
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/team">Team</Link>
       </div>
-    </>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/blog">Blog</Link>
+      <Link to="/team">Team</Link>
+    </div>
   );
 }
