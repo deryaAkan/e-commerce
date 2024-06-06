8c145dcd8a4b8f1b92cdecc6da097ff7d44ce68d
Derya Akan <d.akan369@gmail.com>
2024-06-07 02:33:12 +0300

    chevron fix

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -74,68 +74,64 @@ export default function Slider({
   }, []);
 
   return (
-    <div className="overflow-hidden relative font-Montserrat bg-[#01B6DD]">
-      <div
-        className="flex transition-transform ease-out duration-500 h-[640px]"
-        style={{ transform: `translateX(-${curr * 100}%)` }}
-      >
-        {slides.map((slide, index) => (
-          <div
-            key={index}
-            className="w-full flex justify-center flex-shrink-0 relative "
-          >
-            <img
-              src={slide.img}
-              alt={`Slide ${index}`}
-              className="w-full overflow-hidden sm:object-cover sm:w-fit  "
-            />
-            <div className="absolute top-0 left-0 w-full h-full flex items-center justify-items-start sm:justify-center sm:items-center pl-16 sm:pl-0 ">
-              <div className="text-white sm:items-center text-left sm:text-center flex flex-col gap-8 sm:px-0 px-40">
-                <h2 className="font-bold text-xl">{slide.dateHero}</h2>
-                <h1 className="font-bold sm:text-center text-[58px]">
-                  {slide.title}
-                </h1>
-                <p className="font-normal text-[20px] w-3/5 sm:w-4/6 sm:text-center">
-                  {slide.description}
-                </p>
-                <Link
-                  to="/shop"
-                  className="py-[15px] px-[10px] flex border-solid border-[1px] bg-green rounded-md w-40 justify-center text-lg font-bold tracking-normal"
-                >
-                  SHOP NOW
-                </Link>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className="inset-0 flex items-center justify-between p-4">
+    <div className="relative font-Montserrat bg-[#01B6DD] flex flex-col items-center">
+      <div className="flex justify-center w-full">
         <button
           onClick={prev}
-          className="p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white"
+          className="p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white self-center"
         >
           <ChevronLeft />
         </button>
+        <div
+          className="flex transition-transform ease-out duration-500 h-[640px] overflow-hidden w-full"
+          style={{ transform: `translateX(-${curr * 100}%)` }}
+        >
+          {slides.map((slide, index) => (
+            <div
+              key={index}
+              className="w-full flex justify-center flex-shrink-0 relative"
+            >
+              <img
+                src={slide.img}
+                alt={`Slide ${index}`}
+                className="w-full overflow-hidden sm:object-cover sm:w-fit"
+              />
+              <div className="absolute top-0 left-0 w-full h-full flex items-center justify-items-start sm:justify-center sm:items-center pl-16 sm:pl-0 ">
+                <div className="text-white sm:items-center text-left sm:text-center flex flex-col gap-8 sm:px-0 px-40">
+                  <h2 className="font-bold text-xl">{slide.dateHero}</h2>
+                  <h1 className="font-bold sm:text-center text-[58px]">
+                    {slide.title}
+                  </h1>
+                  <p className="font-normal text-[20px] w-3/5 sm:w-4/6 sm:text-center">
+                    {slide.description}
+                  </p>
+                  <Link
+                    to="/shop"
+                    className="py-[15px] px-[10px] flex border-solid border-[1px] bg-green rounded-md w-40 justify-center text-lg font-bold tracking-normal"
+                  >
+                    SHOP NOW
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
         <button
           onClick={next}
-          className="p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white"
+          className="p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white self-center"
         >
           <ChevronRight />
         </button>
       </div>
-
-      <div className="absolute bottom-4 right-0 left-0 sm:hidden">
-        <div className="flex items-center justify-center gap-2">
-          {slides.map((_, i) => (
-            <div
-              key={i}
-              className={`
-                transition-all w-20 h-3 bg-white 
-                ${curr === i ? "p-2" : "bg-opacity-50"}
-              `}
-            />
-          ))}
-        </div>
+      <div className="flex items-center justify-center gap-2 mt-4">
+        {slides.map((_, i) => (
+          <div
+            key={i}
+            className={`transition-all w-20 h-3 bg-white ${
+              curr === i ? "p-2" : "bg-opacity-50"
+            }`}
+          />
+        ))}
       </div>
     </div>
   );
